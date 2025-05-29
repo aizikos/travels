41e345dd0464001d6c33e2b0fbf136e64e3e6828
Aizirek Abdijalilova <aaizirek499@gmail.com>
2025-05-29 22:22:48 +0600

    changes in header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,11 +1,18 @@
+'use client'
 import React from 'react'
 import scss from './Header.module.scss'
 import Navbar from '../navbar/Navbar'
+import { usePathname } from 'next/navigation';
+
+
 
 const Header = () => {
+  const pathname = usePathname();
+  const isHome = pathname === '/';
+
   return (
-    <div className={scss.Header}>
-        <Navbar/>
+    <div className={isHome ? scss.HeaderWithBackground : scss.Header}>
+      <Navbar />
     </div>
   )
 }
